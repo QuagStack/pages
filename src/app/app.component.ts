af5780ffa15aade1c4233da27ec4c74e2ba73526
QuagStack <62340926+QuagStack@users.noreply.github.com>
2020-05-14 15:13:34 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Copenhagen Sweet Home :)',
+      url: 'https://images.unsplash.com/photo-1513622118278-bc041b3c13ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Looking for the midnight sun.',
+      url: 'https://images.unsplash.com/photo-1505553011163-ed5bb1ae1192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Fjords for Hearts',
+      url: 'https://images.unsplash.com/photo-1473160330398-3aa3dbdf3117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Copenhagen Sweet Home :)',
+      url: 'https://images.unsplash.com/photo-1513622118278-bc041b3c13ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Looking for the midnight sun.',
+      url: 'https://images.unsplash.com/photo-1505553011163-ed5bb1ae1192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Fjords for Hearts',
+      url: 'https://images.unsplash.com/photo-1473160330398-3aa3dbdf3117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Copenhagen Sweet Home :)',
+      url: 'https://images.unsplash.com/photo-1513622118278-bc041b3c13ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Looking for the midnight sun.',
+      url: 'https://images.unsplash.com/photo-1505553011163-ed5bb1ae1192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Fjords for Hearts',
+      url: 'https://images.unsplash.com/photo-1473160330398-3aa3dbdf3117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+    },
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Copenhagen Sweet Home :)',
+      url: 'https://images.unsplash.com/photo-1513622118278-bc041b3c13ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Looking for the midnight sun.',
+      url: 'https://images.unsplash.com/photo-1505553011163-ed5bb1ae1192?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Fjords for Hearts',
+      url: 'https://images.unsplash.com/photo-1473160330398-3aa3dbdf3117?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80'
+    }
+  ]
+  currentPage = 0;
+
+  checkWindowIndex(index : number){
+    return Math.abs(this.currentPage - index) < 5
+  }
+
 }
